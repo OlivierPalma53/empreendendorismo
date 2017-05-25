1deb68b574dd796c6fc3eb3708164796fb7d352e
OlivierPalma <olivierpalmasouza@gmail.com>
2017-05-25 14:24:22 -0300

    Inicio do trabalho

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+  <head>
+	 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Teste de Empreendedorismo</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script>
+
+      function redirecionar(){
+        window.location.href = "perguntas.php";
+      }
+
+    </script>
+  </head>
+
+  <body>
+
+    <div class="capa">
+    	<div class="texto-capa">
+    		<h1>Sejam Bem Vindos ao Teste de Empreendedorismo</h1>
+        <br></br>
+    		<button class="btn btn-custom btn-lg" onclick="redirecionar()">INICIAR TESTE</button><br>
+
+      <a href="index.html#servicos">
+        <div class="arrow">
+          <span><img src="img/arrow.png" width="100px" height="100px"/><a href="#"></a></span>
+        </div>
+    	</div>
+      </a>
+    </div>
+
+    <div class="servicos" id="servicos">
+    	<div class="container">
+    		<div class="row letra">
+
+    				<h3>O que é o Teste de Empreendedorismo?</h3>
+    					<p><span class = "paragrafo"></span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nulla velit, faucibus id massa non, pellentesque vestibulum erat. Mauris nec orci sed leo egestas finibus. Integer maximus diam sed dolor gravida volutpat. Mauris id dapibus nunc, vitae tincidunt nisl. Proin nibh arcu, fringilla nec egestas vel, iaculis vel leo. Phasellus venenatis tortor eu felis interdum, a venenatis leo euismod. Phasellus gravida iaculis dolor eu convallis. Phasellus metus nulla, euismod eget vulputate porta, porttitor ut erat. Morbi euismod eu libero non auctor. Nam ante mi, viverra nec maximus non, maximus id tellus. Duis augue eros, interdum quis tortor non, tincidunt ullamcorper dolor. Pellentesque dictum, metus quis fermentum sollicitudin, nulla dui posuere turpis, fringilla faucibus sapien tellus ut felis.</p>
+              <p><span class = "paragrafo"></span>Fusce suscipit odio et leo pretium, non vestibulum nulla cursus. Aliquam a enim aliquam, pharetra est ut, elementum est. Sed eget facilisis sapien, non sollicitudin ex. Duis maximus vel massa sed volutpat. Ut diam quam, ullamcorper quis dui et, fringilla egestas lectus. Nam ac rutrum augue, a lobortis massa. Donec luctus id neque id consequat. Ut elementum volutpat posuere.</p>
+    		</div>
+    	</div>
+    </div>
+
+	<div id="rodape">
+		<div class="container">
+			<div class="row">
+
+      <div class="col-md-2 rodape2">
+          <ul class="nav">
+            <li class="rede-social" title="Siga-nos no Instagram"> <a href="#"><img src="img/facebook.png"/></a></li>
+            <li class="rede-social" title="Siga-nos no Twitter"> <a href="#"><img src="img/twitter.png"/>  </a></li>
+            <li class="rede-social" title="Siga-nos no Instagram"> <a href="#"><img src="img/instagram.png"/> </a></li>
+          </ul>
+        </div>
+
+				<div class="col-md-2">
+					<h4>PLANEJAMENTO</h4>
+					<ul class="nav nav-rodape">
+						<p>Dábrio</p>
+            <p>Gustavo</p>
+            <p>Geovane</p>
+            <p>Andreia</p>
+            <p>Suelen</p>
+            <p>Weverton</p>
+            <p>Lara</p>
+            <p>Maria</p>
+					</ul>
+				</div>
+
+        <div class="col-md-2">
+          <h4>DESIGN</h4>
+          <ul class="nav nav-rodape">
+            <p>Henrique Brandão</p>
+            <p>John Cunha</p>
+            <p>João Vitor Soares</p>
+            <p>Letícia</p>
+            <p>Manuella Brandão</p>
+            <p>Matheus José</p>
+          </ul>
+        </div>
+
+        <div class="col-md-2">
+          <h4>DESENVOLVEDORES</h4>
+          <ul class="nav nav-rodape">
+            <p>Cristtiano Witor</p>
+            <p>Matheus da Costa</p>
+            <p>Mayquel Phílipe</p>
+            <p>Vitor Lepesqueur</p>
+          </ul>
+        </div>
+
+			</div>
+		</div>
+	</div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
